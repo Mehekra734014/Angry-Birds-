2ecc6b8ea98f57f202476928829322faaad3ac3d
Mehek Daga <80312287+Mehekra734014@users.noreply.github.com>
2021-08-26 17:29:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -101,7 +101,7 @@ function keyPressed(){
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Dubai");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
